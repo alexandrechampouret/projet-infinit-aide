9061772755879ea15b7851720f4557ee926ef6d3
alexandrechampouret <champouret@laposte.net>
2022-03-24 20:20:19 +0100

    article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
@@ -21,7 +25,6 @@ class ArticleController extends AbstractController
     /**
      * @Route("/listarticles", name= "listarticles")
      */
-
     public function listarticles(){
         // on accésde a la base de donnée d'article et on recupére tout les articles . 
         $articles = $this->getDoctrine()->getManager()->getRepository('App:Article')->findAll();
@@ -30,4 +33,49 @@ class ArticleController extends AbstractController
 
         return $this->render('article/index.html.twig', $args);
     }
+
+    /**
+     * @Route("/addarticle", name= "addarticle")
+     */
+
+    public function addarticle(Request $request, EntityManagerInterface $entityManagerInterface){
+        $user = $this->get('session')->get('user');
+        $formulaire = $this->createForm(ArticleType::class);
+        // Pour envoyer le formulaire (et ce qui apprait dans le bouton)
+        $formulaire->add('formulaire_dinscription', SubmitType::class, ['label' => 'Ajouter']);
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            // On recupére tout les données saisi par les  utilisateurs 
+            $articleForm = $formulaire->getData();
+            $articleBd = $entityManagerInterface->getRepository('App:Article')->findOneBy(array('titre' => $formulaire->get('titre')->getData())); 
+            // On vérifie l'exsistance de l'article
+            if($articleBd){
+                // On modifie le contenu de l'article
+                $articleBd->setDescription($formulaire->get('description')->getData());
+            }else{
+                //si n'exsiste pas on ajoute l'intégralité dans le form. 
+                $entityManagerInterface->persist($articleForm);
+            }
+            
+            $entityManagerInterface->flush();
+            // On a joute un message de confirmation
+            $this->addFlash("success", "Votre article a bien était créé");
+            // On redirige vers la même page 
+            $this->redirectToRoute('home');
+    
+
+        }
+        if ($formulaire->isSubmitted()){
+            // On gére les erreurs 
+            $this->addFlash("error", "Erreur dans le Formulaire");
+        }
+
+        $args = array('form' => $formulaire->createView(), 'user' => $user);
+
+        return $this->render('article/ajoutArticle.html.twig', $args);
+        
+    }
+
+    
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,45 +30,53 @@ class UserController extends AbstractController
     */
     public function inscription(Request $request, EntityManagerInterface $entityManagerInterface, UserPasswordHasherInterface $userPasswordHasher)
     {
-
-        $formulaire = $this->createForm(UserType::class);
-        // Pour envoyer le formulaire (et ce qui apprait dans le bouton)
-        $formulaire->add('formulaire_dinscription', SubmitType::class, ['label' => 'Envoyer']);
-        $formulaire->handleRequest($request);
-        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-            // On recupére tout les données saisi par les  utilisateurs 
-            $utilisateur = $formulaire->getData();
-            // On hash le mot de passe
-            $utilisateur->setPassword(
-                $userPasswordHasher->hashPassword(
-                        $utilisateur,
-                        $formulaire->get('password')->getData()
-                    )
-                );
-
-            // autre methode de hashage 
-            // $utilisateur->setPassword(sha1($formulaire->get('motdepasse')->getData()));
-
-            // on donne notre variable a entity manager et flush-> pousse dans la BDD 
-            $entityManagerInterface->persist($utilisateur);
-            $entityManagerInterface->flush();
-            // On a joute un message de confirmation
-            $this->addFlash("success", "Votre a bien était créé");
-            // On redirige vers la même page 
-            $this->redirectToRoute('home');
-    
-
+        // On teste si anonyme
+        $user = $this->get('session')->get('user');
+        $utilisateur = $this->getUserDB($user);
+ 
+        if($utilisateur == null) {
+            $formulaire = $this->createForm(UserType::class);
+            // Pour envoyer le formulaire (et ce qui apprait dans le bouton)
+            $formulaire->add('formulaire_dinscription', SubmitType::class, ['label' => 'Envoyer']);
+            $formulaire->handleRequest($request);
+            if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+                // On recupére tout les données saisi par les  utilisateurs 
+                $utilisateur = $formulaire->getData();
+                // On hash le mot de passe
+                $utilisateur->setPassword(
+                    $userPasswordHasher->hashPassword(
+                            $utilisateur,
+                            $formulaire->get('password')->getData()
+                        )
+                    );
+ 
+                // autre methode de hashage 
+                // $utilisateur->setPassword(sha1($formulaire->get('motdepasse')->getData()));
+ 
+                // on donne notre variable a entity manager et flush-> pousse dans la BDD 
+                $entityManagerInterface->persist($utilisateur);
+                $entityManagerInterface->flush();
+                // On a joute un message de confirmation
+                $this->addFlash("success", "Votre a bien était créé");
+                // On redirige vers la même page 
+                $this->redirectToRoute('home');
+        
+ 
+            }
+            if ($formulaire->isSubmitted()){
+                // On gére les erreurs 
+                $this->addFlash("error", "Erreur Inscription");
+            }
+ 
+            // RENDU
+            return $this->render('user/register.html.twig', [
+                'user' => $user,
+                'form' => $formulaire->createView(),
+            ]);
+        } else {
+            throw $this->createNotFoundException('Cette page n\'est accessible que par les utilisateurs non inscrits');
         }
-        if ($formulaire->isSubmitted()){
-            // On gére les erreurs 
-            $this->addFlash("error", "Erreur Inscription");
-        }
-
-        // RENDU
-        return $this->render('user/inscription.html.twig', [
-            // 'user' => $user,
-            'form' => $formulaire->createView(),
-        ]);
+        
     }
 
     /** 
@@ -77,35 +85,29 @@ class UserController extends AbstractController
     public function connexion(Request $request, EntityManagerInterface $entityManagerInterface, UserPasswordHasherInterface $userPasswordHasher)
     {
         
+        
         //récupere le user présent dans la session 
         $userBd = $this->get('session')->get('user');
         
-
+ 
         if ($userBd == null){
-
+ 
             
             $formulaire = $this->createForm(LoginType::class);
             // Pour envoyer le formulaire (et ce qui apprait dans le bouton)
             $formulaire->add('formulaire_connexion', SubmitType::class, ['label' => 'Connexion']);
             $formulaire->handleRequest($request);
             if ($formulaire->isSubmitted() && $formulaire->isValid()) {
-                // On recupére tout les données saisi par les  utilisateurs 
-                $utilisateur = $formulaire->getData();
-
                 // on recupére l'email saisi par l'utilisateur 
-                $userBd = $this->getUser($formulaire->get('email')->getData());
-                
-                // On hash le mot de passe saisi par l'user
-                $mdpHacher = $userPasswordHasher->hashPassword(
-                    $utilisateur,
-                    $formulaire->get('password')->getData()
-
-                );
-                //tu vas chercher la variable userBd = elle vient de l'entity (type) User 
-                /** @var $userBd User */
-                if ($userBd->getPassword() == $mdpHacher){
+                $userBd = $this->getUserDB($formulaire->get('email')->getData());
+                $mdpFormulaire = $formulaire->get('password')->getData();
+ 
+                // Vérifier si le mdp est correct
+                $isMdpCorrect = $userPasswordHasher->isPasswordValid($userBd, $mdpFormulaire);
+                if($isMdpCorrect) {
 
                     // session = variable globale et je stock l'email de l'utilisateur 
+                    $this->startSession();
                     $this->get('session')->set('user', $userBd->getEmail());
                 }
                 else{
@@ -114,9 +116,10 @@ class UserController extends AbstractController
                     return $this->redirect($request->getUri());
                 }
                 $this->addFlash('succes', 'Vous êtes bien connecté');
+                return $this->redirectToRoute('home');
         
             }
-
+ 
             // RENDU
             return $this->render('user/login.html.twig', [
                 'user' => $userBd,
@@ -124,36 +127,50 @@ class UserController extends AbstractController
             ]);
         }
         else{
-
+ 
             // test de la fonction
             $this->addFlash('error', 'Vous etes déjà authentifié.');
             return $this->render('user/index.html.twig', array('user' => $userBd));
         }
     }
-
-
-    // ?object = objet qui contient plusieurs type
-    // elle retourne tout les donnée d'un user dans la base donnée . 
-    // public function getUser($email){
-
-    //     // on retourne un objet de type user récupérer sur la base de donnée. et chercher avec le mail 
-    //     // Doctrine : librairie qui la base de donnée 
-    //     // manager entity manager 
-        
-    //     return $this->getDoctrine()->getManager()
-    //         ->getRepository('App:User')
-    //         ->findOneBy(array('email'=>$email));
-    // }
-    
+ 
     /** 
     * @Route("/logout", name= "logout")
     */
-
-    public function logout (){
-        $this->get('session')->set('user', '');
-        $this->addFlash('logout', 'Vous êtes déconnecté');
-
+    public function logout() {
+        $this->stopSession($this->get('session'));
+ 
+        $this->addFlash('success', 'Vous etes déconnecté.');
         return $this->redirectToRoute('home');
+
+    }
+ 
+    
+    // elle retourne tout les donnée d'un user dans la base donnée . 
+    public function getUserDB($email) {
+
+        // on retourne un objet de type user récupérer sur la base de donnée. et chercher avec le mail 
+        // Doctrine : librairie qui la base de donnée 
+        // manager entity manager 
+        return $this->getDoctrine()->getManager()
+            ->getRepository('App:User')
+            ->findOneBy(array('email'=>$email));
+    }
+    // Créer/détruire une session
+    public function startSession() {
+        
+        $session = $this->get('session');
+        if(!isset($session)) {
+            $session->start();
+        }
+    }
+
+    public function stopSession(Session $session) {
+        // $session = new Session();
+        $session->clear();
+        //pour detruire la session "session plus valide" 
+        $session->invalidate(1);
+ 
     }
 
 
